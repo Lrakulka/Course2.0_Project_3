42683ac13d1854d13e0d4bbfabd2abb8b2309e96
Lrakulka <olecsandr32@gmail.com>
2016-05-08 20:57:35 +0300

    DOM parser finished

--- src/com/homework/analyzer/Util.java
+++ src/com/homework/analyzer/Util.java
@@ -6,14 +6,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
 import gem_xsd.JewelType;
+import gem_xsd.JewelType.VisualParameters;
 
 public class Util {
     /**
@@ -58,7 +65,80 @@ public class Util {
      */
     public static List<JewelType> initListDOM(String xmlPath) {
 	List<JewelType> list = new ArrayList<>();
-	
+	JewelType jewel;
+	try {
+    	    File inputFile = new File(xmlPath);
+            DocumentBuilderFactory dbFactory = 
+               DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder;
+    	    dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            NodeList jewelList = doc.getElementsByTagName("jewel");
+            NodeList jewelChildrList, jawelVisualParametersList;
+            Node jawelNode, jawelChildNode;
+            for (int i = jewelList.getLength() - 1; i > -1; --i) {
+        	jawelNode = jewelList.item(i);
+        	jewel = new JewelType();
+        	if (jawelNode.getNodeType() == Node.ELEMENT_NODE) {
+        	    jewel.setId(((Element) jawelNode).getAttribute("id"));
+        	}
+        	jewelChildrList = jawelNode.getChildNodes();
+        	for(int j = jewelChildrList.getLength() - 1; j > -1; --j) {
+        	    jawelChildNode = jewelChildrList.item(j);
+                    if (jawelChildNode.getNodeType() == Node.ELEMENT_NODE) {
+                	Element element = (Element) jawelChildNode;
+                	switch (element.getNodeName()) {
+                	case "Name":
+                		jewel.setName(element.getTextContent());
+                		break;
+                	case "Preciousness":
+                		jewel.setPreciousness(Boolean.
+                			parseBoolean(element.getTextContent()));
+                		break;
+                	case "Origin":
+                	    	jewel.setOrigin(element.getTextContent());
+                	    	break;
+                	case "Value":
+                	    	jewel.setValue(Double.parseDouble(element.getTextContent()));
+                	    	break;
+                	case "Visual_Parameters":
+                	    jawelVisualParametersList = jawelChildNode.getChildNodes();
+                	    VisualParameters parameters = new VisualParameters();
+                	    for (int k = jawelVisualParametersList.getLength() - 1; k > -1; --k) {
+                		if (jawelVisualParametersList.item(k).getNodeType() 
+                			== Node.ELEMENT_NODE) {
+                        	   Element visualParameters = (Element) jawelVisualParametersList.item(k);
+                        	   switch (visualParameters.getNodeName()) {
+                        	   case "Color":
+    				      parameters.setColor(visualParameters.getTextContent());
+    				      break;
+        			   case "Transparency":
+        			       parameters.setTransparency(Byte.parseByte(visualParameters.getTextContent()));
+        			       break;
+        			   case "Cut":
+        			       parameters.setCut(Byte.parseByte(visualParameters.getTextContent()));
+        			       break;
+                        	   }
+                		}
+                	    }     
+                	    jewel.setVisualParameters(parameters);
+                	    break;
+                	}
+                    }
+        	}
+        	list.add(jewel);
+            }
+	} catch (ParserConfigurationException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (SAXException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
 	return list;
     }
     /**
